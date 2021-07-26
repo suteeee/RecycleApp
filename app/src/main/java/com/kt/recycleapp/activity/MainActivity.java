34bfe55024fbce758eed23d6ad4553a3f3aba0f9
이주광 <sute__@naver.com>
2021-07-26 15:29:22 +0900

    RecycleApp

--- app/src/main/java/com/kt/recycleapp/activity/MainActivity.java
+++ app/src/main/java/com/kt/recycleapp/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.kt.recycleapp;
+package com.kt.recycleapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.kt.recycleapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
