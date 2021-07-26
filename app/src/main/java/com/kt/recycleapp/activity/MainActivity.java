9760ab2bcccb69ba189dce739a33967fc2dd9e13
이주광 <sute__@naver.com>
2021-07-26 15:31:12 +0900

    RecycleApp

--- app/src/main/java/com/kt/recycleapp/activity/MainActivity.java
+++ app/src/main/java/com/kt/recycleapp/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.kt.recycleapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.kt.recycleapp.R;
 
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("d","d");
     }
 }
