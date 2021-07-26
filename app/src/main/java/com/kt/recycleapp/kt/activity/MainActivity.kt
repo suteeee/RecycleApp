165674daff89af2bb9c9a7b3be210752d1ab8573
이주광 <sute__@naver.com>
2021-07-26 22:19:51 +0900

    RecycleApp 그래들 수정, 드로어 , 네비게이션뷰 추가

--- app/src/main/java/com/kt/recycleapp/kt/activity/MainActivity.kt
+++ app/src/main/java/com/kt/recycleapp/kt/activity/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.kt.recycleapp.kt.activity
 
 import android.os.Bundle
-import android.view.Gravity
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import com.google.android.material.navigation.NavigationView
+import androidx.fragment.app.Fragment
+import com.kt.recycleapp.kt.fragment.FindFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import java.recycleapp.R
 
@@ -22,17 +22,24 @@ class MainActivity : AppCompatActivity() {
         actionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
 
         navi_nv.setNavigationItemSelectedListener { menuItem ->
+            var fragment : Fragment? = null
+
+
             menuItem.isChecked = true
             drawer_layout.closeDrawers()
             val id = menuItem.itemId
             val title = menuItem.title.toString()
             if (id == R.id.t) {
-                Toast.makeText(this, "$title: 14", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "$title: find", Toast.LENGTH_SHORT).show()
+                fragment = FindFragment()
             } else if (id == R.id.tt) {
                 Toast.makeText(this, "$title: 126.", Toast.LENGTH_SHORT).show()
+                //fragment = A
             } else if (id == R.id.ttt) {
                 Toast.makeText(this, "$title: 5465", Toast.LENGTH_SHORT).show()
             }
+
+            supportFragmentManager.beginTransaction().replace(R.id.container_layout1,fragment!!).commit()
             true
         }
     }
