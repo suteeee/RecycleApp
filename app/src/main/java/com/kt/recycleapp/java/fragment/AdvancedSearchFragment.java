99d6f5aad038df3e2da2fa25b134e10775bfa149
이주광 <sute__@naver.com>
2021-07-27 21:17:24 +0900

    메인 액티비티 디자인 1차 수정 3

--- app/src/main/java/com/kt/recycleapp/java/fragment/AdvancedSearchFragment.java
+++ app/src/main/java/com/kt/recycleapp/java/fragment/AdvancedSearchFragment.java
@@ -10,11 +10,7 @@ import android.view.ViewGroup;
 
 import java.recycleapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdvancedSearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AdvancedSearchFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
