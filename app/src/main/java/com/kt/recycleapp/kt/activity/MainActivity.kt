3cff153018bd819c9c36654ede515b2d85010775
이주광 <sute__@naver.com>
2021-07-27 20:54:15 +0900

    메인 액티비티 디자인 1차 수정 3

--- app/src/main/java/com/kt/recycleapp/kt/activity/MainActivity.kt
+++ app/src/main/java/com/kt/recycleapp/kt/activity/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.kt.recycleapp.kt.activity
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode
@@ -19,13 +26,35 @@ import com.kt.recycleapp.kt.fragment.FavoriteItemFragment
 import com.kt.recycleapp.kt.fragment.FindFragment
 import com.kt.recycleapp.kt.fragment.RecycleDayInfoFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.lang.Exception
 import java.recycleapp.R
+import java.recycleapp.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    private var preview: Preview? = null
+    private var imageCapture:ImageCapture? = null
+    private lateinit var outputDirectory:File
+    private lateinit var cameraExecutor:ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        cameraSet()
+
+        binding.captureBtn.setOnClickListener { takePhoto() }
+
+        outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
 
         setSupportActionBar(mainToolBar_tb1)
         val actionBar =supportActionBar
@@ -95,9 +124,63 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    fun cameraSet() {
-        val preview = Preview.Builder().build()
-        //val viewFinder :PreviewView =
+    private fun cameraSet() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider:ProcessCameraProvider = cameraProviderFuture.get()
+            preview = Preview.Builder().build()
+                .also {
+                    it.setSurfaceProvider(binding.previewView.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try{
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            }catch (e:Exception){
+                Log.d("cameraX","Binding failed")
+            }
+
+        },ContextCompat.getMainExecutor(this))
+
+
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture?:return
+        val photoFile = File(outputDirectory,newJpgFileName())
+        val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(outputOption,ContextCompat.getMainExecutor(this),object:ImageCapture.OnImageSavedCallback{
+            override fun onError(exception: ImageCaptureException) {
+                Log.d("cameraX","captureFail")
+            }
+
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                val saveUri = Uri.fromFile(photoFile)
+                val msg = "Success:$saveUri"
+                Toast.makeText(baseContext,msg,Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun newJpgFileName():String{
+        val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
+        val filename = "Recycle_${sdf.format(System.currentTimeMillis())}"
+        return "${filename}.jpg"
+    }
+
+    private fun getOutputDirectory():File{
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it,resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if(mediaDir != null && mediaDir.exists()) mediaDir
+        else filesDir
     }
 
     fun barcodeOption() {
@@ -106,5 +189,10 @@ class MainActivity : AppCompatActivity() {
                 FirebaseVisionBarcode.FORMAT_EAN_8,
                 FirebaseVisionBarcode.FORMAT_EAN_13)
             .build()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
